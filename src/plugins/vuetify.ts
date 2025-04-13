949a98b6b79cdd3545f16f178c8db93ec0c2edad
andrew <chuckk589@gmail.com>
2025-04-14 01:13:37 +0300

    feat: vuetify themes

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,19 +4,43 @@ import 'vuetify/dist/vuetify.min.css';
 
 // Vuetify
 import { createVuetify } from 'vuetify';
+import { ThemeDefinition } from 'vuetify/lib/types';
+const agrotechsim: ThemeDefinition = {
+    dark: false,
+    colors: {
+        "primary-base": "#38c793",
+        "primary-base-dark": "#38c793",
+        "on-background": "#FFFFFF",
+        "on-surface": "#FFFFFF",
+    },
+    variables: {
+        'high-emphasis-opacity': 1,
+    }
+}
 
+const droneverse: ThemeDefinition = {
+    dark: false,
+    colors: {
+        "primary-base": "#f17b2c",
+        "primary-base-dark": "#f17b2c",
+        "on-background": "#FFFFFF",
+        "on-surface": "#FFFFFF",
+    },
+    variables: {
+        'high-emphasis-opacity': 1,
+    }
+}
 export default createVuetify({
-  defaults: {
-    VTextarea: {
-      density: 'compact'
+    defaults: {
+        VBtn: {
+            variant: 'tonal'
+        }
     },
-    VSelect: {
-      density: 'compact'
+    theme: {
+        defaultTheme: 'agrotechsim',
+        themes: {
+            agrotechsim,
+            droneverse,
+        },
     },
-    VTextField: {
-      density: 'compact'
-    },
-   
-  },
-
 });
