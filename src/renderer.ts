0b9ac401cd8be6052e9994e78473f697f37e7fee
andrew <chuckk589@gmail.com>
2025-04-14 01:15:16 +0300

    ref: move stores initialization to app.vue

--- src/renderer.ts
+++ src/renderer.ts
@@ -3,24 +3,14 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import { router } from './router/index';
-import { useErrorStore } from './stores/errorStore';
-import { useManagerStore } from './stores/managerStore';
-import { useProductStore } from './stores/productStore';
-// import { useVersionStore } from './stores/version';
-// import './styles/main.scss';
+import './styles/main.scss';
+import './styles/settings.scss';
+import './styles/typography.scss';
 
 const app = createApp(App);
 app.use(vuetify);
 app.use(createPinia());
 app.use(router);
 
-const errorStore = useErrorStore()
-errorStore.subscribeToErrors()
 
-const productStore = useProductStore()
-productStore.initializeStore()
-// const versionStore = useVersionStore()
-// versionStore.initializeStore()
-const managerStore = useManagerStore()
-managerStore.initializeStore()
 app.mount('#app');
