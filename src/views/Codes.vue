741f0fc506e63f4233a5a147a2f75a0d6b1538dc
andrew <chuckk589@gmail.com>
2025-04-17 17:49:52 +0300

    fix: license type

--- src/views/Codes.vue
+++ src/views/Codes.vue
@@ -21,13 +21,13 @@
     </div>
     <div class="at-code-history ">
       <div class="text-average">История активаций</div>
-      <v-data-table :headers="headers" hide-default-header hide-default-footer
+      <v-data-table no-data-text="История активация пуста" :headers="headers" hide-default-header hide-default-footer
         class="at-code-table text-medium text-soft-300" :items="history">
-        <template v-slot:item.id="{ item }">
-          <span>id{{ item.id }}</span>
+        <template v-slot:item.licenseId="{ item }">
+          <span>id{{ item.licenseId }}</span>
         </template>
-        <template v-slot:item.timestamp="{ item }">
-          <span>{{ new Date(item.timestamp).toLocaleString() }}</span>
+        <template v-slot:item.activationDate="{ item }">
+          <span>{{ new Date(item.activationDate).toLocaleString() }}</span>
         </template>
       </v-data-table>
     </div>
@@ -39,20 +39,17 @@ import { STORE_VERSION } from '@/db/constants';
 import { GuardantStatus } from '@/guardant.types';
 import { useCacheStore } from '@/stores/cacheStore';
 import { useErrorStore } from '@/stores/errorStore';
-import { useManagerStore } from '@/stores/managerStore';
-import { useProductStore } from '@/stores/productStore';
 import { ProductCachedMetadata } from '@/types';
 import { EventScope } from '@/utils/errorHandler';
 import { onMounted, ref } from 'vue';
+
 const form = ref(null);
 const loading = ref(false);
 const code = ref('');
 const history = ref([]);
 const errorStore = useErrorStore();
 const cacheStore = useCacheStore();
-const headers = [{ value: 'id' }, { value: 'name' }, { value: 'timestamp' }];
-const managerStore = useManagerStore()
-const productStore = useProductStore()
+const headers = [{ value: 'licenseId' }, { value: 'code' }, { value: 'activationDate' }];
 
 const checkCode = async (code: string) => {
   return await window.guardant.method('checkSerialNumberFormat', code);
@@ -73,11 +70,7 @@ const activateCode = async () => {
 
       if (response.status == GuardantStatus.OK) {
 
-        await managerStore.refreshInstalledProducts()
-
-        await cacheStore.updateProductMetaData({ licenseId: response.licenseId, activationDate: Date.now() })
-
-        await productStore.fetchProducts()
+        await cacheStore.updateProductMetaData({ licenseId: response.licenseId, code: code.value, activationDate: Date.now() })
 
         loadHistory()
       }
@@ -89,16 +82,7 @@ const activateCode = async () => {
 }
 
 const loadHistory = async () => {
-  const historyRaw = await cacheStore.getCachedData<ProductCachedMetadata[]>(STORE_VERSION, 'products');
-
-  history.value = historyRaw?.map((item) => {
-    const product = productStore.products.find((product) => product.license.licenseId === item.licenseId);
-    return {
-      id: item.licenseId,
-      timestamp: item.activationDate,
-      name: product?.label || 'Неизвестно',
-    }
-  });
+  history.value = await cacheStore.getCachedData<ProductCachedMetadata[]>(STORE_VERSION, 'products');
 }
 onMounted(() => {
   loadHistory()
